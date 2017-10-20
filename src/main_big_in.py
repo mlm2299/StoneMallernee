646d669bf36da66ddd58616e5eb22c253ea3e4e9
Maggie <mlm2299@columbia.edu>
2017-10-20 11:18:17 -0400

    Added main_big_in to produce a single big.in file using file_o_projects

---
+++ src/main_big_in.py
@@ -0,0 +1,9 @@
+'''
+File to solely generate a big.in file
+@author Maggie Mallernee
+December 1, 2016
+'''
+
+import file_o_projects as get_big_in
+
+get_big_in.frame_format('JUPITER', 10000000.0, 10000000.0, 100.0, 100.0)     #name, m_cen, m_big, a_big, r_start -- equality of last two indicates eccentricity of 0
